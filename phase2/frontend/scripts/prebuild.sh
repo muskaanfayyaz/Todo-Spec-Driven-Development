3a508fee064c065cef5c6081ad49cc74761be5a0
Muskaan Fayyaz <muskaanfayyaz3@gmail.com>
2026-02-10 01:37:20 +0500

    updated

--- phase2/frontend/scripts/prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Clean up any previous copy
-rm -rf ./components/chat
-
-# Copy the chat components from phase3 to phase2
-cp -r ../../phase3/frontend/components/chat ./components/chat
