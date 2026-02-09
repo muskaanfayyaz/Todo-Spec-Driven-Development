e68d5b21075efb64e63b6424f197da8438478692
Muskaan Fayyaz <muskaanfayyaz3@gmail.com>
2026-02-10 01:32:25 +0500

    updated

--- phase2/frontend/next.config.js
+++ phase2/frontend/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    externalDir: true,
-  },
-  transpilePackages: ['@phase3/frontend'],
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '',
