30d7b971e74c25b4c9957933211575375469844c
Muskaan Fayyaz <muskaanfayyaz3@gmail.com>
2026-02-09 16:58:14 +0500

    fixed "Module not found" error

--- phase2/frontend/next.config.js
+++ phase2/frontend/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    externalDir: true,
+  },
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '',
